0df540c2388dd68642e4cd6d9f2ec22420bd7985
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2015-05-31 04:10:24 +0200

    Bundle libevent

--- config.h.cmake
+++ config.h.cmake
@@ -681,4 +681,30 @@
 #error <my_config.h> MUST be included first!
 #endif
 
+/* Libevent */
+#cmakedefine HAVE_DEVPOLL 1
+#cmakedefine HAVE_SYS_DEVPOLL_H 1
+#cmakedefine HAVE_SYS_EPOLL_H 1
+#cmakedefine HAVE_TAILQFOREACH 1
+
+/*
+  Hardcoded values needed by libevent/NDB/memcached
+  TODO: These definitions should be cleaned and transformed into
+  proper checks.
+*/
+#define HAVE_FCNTL_H 1
+#define HAVE_GETADDRINFO 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_SELECT 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRDUP 1
+#define HAVE_STRTOK_R 1
+#define HAVE_STRTOLL 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define SIZEOF_CHAR 1
+
 #endif
