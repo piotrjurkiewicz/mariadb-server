79bcdb11685627274235784f14abc4fd9435b6d7
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2015-07-04 02:17:49 +0200

    Move and rename demo table SQL script file name
    Move script file with a demo table initialization commands from
    'innodb_memcache' to 'daemon_memcached' directory.
    
    Rename script file from 'innodb_memcached_config.sql' to
    'daemon_memcached_config.sql'.

--- plugin/daemon_memcached/innodb_memcache/src/innodb_config.c
+++ plugin/daemon_memcached/innodb_memcache/src/innodb_config.c
@@ -523,7 +523,7 @@ innodb_config_meta_hash_init(
 	if (err != DB_SUCCESS) {
 		fprintf(stderr, " InnoDB_Memcached: Please create config table"
 				"'%s' in database '%s' by running"
-				" 'innodb_memcached_config.sql. error %s'\n",
+				" 'daemon_memcached_config.sql. error %s'\n",
 			MCI_CFG_CONTAINER_TABLE, MCI_CFG_DB_NAME,
 			ib_cb_ut_strerr(err));
 		err = DB_ERROR;
@@ -640,7 +640,7 @@ innodb_config_container(
 	if (err != DB_SUCCESS) {
 		fprintf(stderr, " InnoDB_Memcached: Please create config table"
 				"'%s' in database '%s' by running"
-				" 'innodb_memcached_config.sql. error %d'\n",
+				" 'daemon_memcached_config.sql. error %d'\n",
 			MCI_CFG_CONTAINER_TABLE, MCI_CFG_DB_NAME,
 			err);
 		err = DB_ERROR;
