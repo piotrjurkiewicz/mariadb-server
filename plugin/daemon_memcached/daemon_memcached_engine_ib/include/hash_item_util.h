8d7faed9653fa871381c4378249e55d7f0f4d9bc
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2015-08-10 23:36:33 +0200

    Remove unused header files from memcached engine_ib

--- plugin/daemon_memcached/daemon_memcached_engine_ib/include/hash_item_util.h
+++ plugin/daemon_memcached/daemon_memcached_engine_ib/include/hash_item_util.h
@@ -7,8 +7,6 @@
 
 #include <memcached/engine.h> 
 
-#include "dbmemcache_global.h"
-
 #define ITEM_WITH_CAS 1
 
 struct default_engine;   // forward reference; needed in items.h
@@ -30,7 +28,9 @@ typedef struct _hash_item {
     uint8_t slabs_clsid;/* which slab class we're in */
 } hash_item;
 
-DECLARE_FUNCTIONS_WITH_C_LINKAGE
+#ifdef    __cplusplus
+extern "C" {
+#endif
 
 uint16_t hash_item_get_key_len(const hash_item *item);
 uint32_t hash_item_get_data_len(const hash_item *item);
@@ -43,6 +43,8 @@ uint64_t * hash_item_get_cas_ptr(const hash_item* item);
 void	hash_item_set_flag(hash_item* item, uint32_t value);
 void	hash_item_set_cas(hash_item* item, uint64_t cas);
 
-END_FUNCTIONS_WITH_C_LINKAGE
+#ifdef    __cplusplus
+}
+#endif
 
 #endif
